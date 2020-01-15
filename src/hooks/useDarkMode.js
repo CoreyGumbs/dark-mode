1e0ab89f1029e0b0442c3e5da4672d80b65862e6
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-15 18:38:27 -0500

    [Add] Completed Darkmode Hook

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,6 +1,21 @@
-import React from 'react';
-
+import {useEffect} from 'react';
+import useLocalStorage from './useLocalStorage';
 
 const useDarkMode = () => {
-    
+    const [darkMode, setDarkMode] = useLocalStorage('dark');
+
+    useEffect(()=>{
+        const body = document.querySelector('body');
+
+        if(darkMode){
+            body.add('dark-mode');
+        }else{
+            body.remove('dark-mode');
+        }
+
+    },[darkMode]);
+
+    return [darkMode, setDarkMode];
 }
+
+export default useDarkMode;
