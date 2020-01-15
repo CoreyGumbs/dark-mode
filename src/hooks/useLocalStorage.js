5fd619fa421d4415f6bf7daa991b8f2d6468768f
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-15 18:04:33 -0500

    [add] added callback function to storedValue useState Hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
 
+    return [storedValue];
 }
